16a7c4e7de8888f24801a94ad8ed95067804a264
hchiam <hchiam@users.noreply.github.com>
2021-01-10 20:01:58 -0500

    other possible options

--- more-sophisticated-build.js
+++ more-sophisticated-build.js
@@ -1,7 +1,10 @@
 require("esbuild")
   .build({
     entryPoints: ["app.jsx"],
-    bundle: true,
     outfile: "out.js",
+    bundle: true,
+    // minify: true,
+    // sourcemap: true,
+    // target: ['chrome58', 'firefox57', 'safari11', 'edge16'],
   })
   .catch(() => process.exit(1));
